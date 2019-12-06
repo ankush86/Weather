cc3e548ecad070779ce8d8e4b195bd74f2d461a9
Ankush <ankushkatar796@gmail.com>
2019-12-06 15:03:05 +0530

    Add note into temperature_notes

--- app/services/city_temperature_service.rb
+++ app/services/city_temperature_service.rb
@@ -1,7 +1,6 @@
 require 'forecast_io'
 
 class CityTemperatureService
-
   def initialize(destinations)
     ForecastIO.api_key = Rails.application.secrets.forecast_io_key
     @destinations = destinations
@@ -12,17 +11,32 @@ class CityTemperatureService
     @destinations.each do |dest|
       # get data to table for cache 1 hour
       forecast = get_temperature_record(dest)
+      city = dest.city_name
+      temp = forecast.temperature
+
       sleep 5
-      puts "Creating TemperatureNote for #{dest.city_name}"
-      dest.temperature_notes.create!(time: current_time,
-                                     temperature: forecast.temperature,
-                                     note: '') # need to add note
+      puts "Creating TemperatureNote for #{city}"
+      dest.temperature_notes.create!(time: @current_time,
+                                     temperature: temp,
+                                     note: get_note(city, temp))
     end
   end
+
+  private
 
   def get_temperature_record(dest)
     ForecastIO.forecast(dest.latitude, dest.longitude,
                         time: @current_time.to_i,
                         params: { units: 'si' })['currently']
+  end
+
+  def get_note(_city, temp)
+    if temp > 25
+      "Let's go for a pint"
+    elsif temp < 5
+      '!Que frio!'
+    else
+      'Its wonderful day'
+    end
   end
 end
