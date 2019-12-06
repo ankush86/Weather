06056ab6e576b25329db8b2f55c26d659ddc15f1
Ankush <ankushkatar796@gmail.com>
2019-12-06 14:35:02 +0530

    Create methods and add comment

--- app/services/destination_service.rb
+++ app/services/destination_service.rb
@@ -6,24 +6,47 @@ class DestinationService
 
   def create_city_record
     city_names = []
-    # get destination city names
+    # Get destination city names
     @data.css('.detail-table__cell.text-uppercase.fdabf-td2 .visible-xs').each { |dest| city_names << dest.text }
 
     city_names.each do |city|
       unless Destination.all.map(&:city_name).include? city
-        # get coordinate of city
-        result = Geocoder.search(city).first
-        sleep(10)
-        if result.present?
-          lat = result.data['lat']
-          lon = result.data['lon']
-
-          Destination.create(city_name: city, latitude: lat, longitude: lon)
-          puts "create record for #{city}"
-        else
-          puts "Not get coordinate of #{city}"
-        end
+        # Create city record with coordinate
+        create_destination(city)
       end
     end
+    create_temperature_notes(city_names)
+  end
+
+  private
+
+  def create_destination(city)
+    coordinate = get_coordinate(city)
+    if coordinate.present?
+      lat = coordinate[:lat]
+      lon = coordinate[:lon]
+
+      Destination.create(city_name: city, latitude: lat, longitude: lon)
+      puts "create record for #{city}"
+    else
+      puts "Not get coordinate of #{city}"
+    end
+  end
+
+  def get_coordinate(city)
+    result = Geocoder.search(city).first
+    sleep(10)
+    if result.present?
+      lat = result.data['lat']
+      lon = result.data['lon']
+      { lat: lat, lon: lon }
+    end
+  end
+
+  def create_temperature_notes(city_names)
+    # Get newly destination of flight
+    new_destinations = Destination.where(city_name: city_names)
+    # Create temperature record of that
+    CityTemperatureService.new(new_destinations).create_temperature_note
   end
 end
--- lib/tasks/vienna_airport.rake
+++ lib/tasks/vienna_airport.rake
@@ -3,12 +3,11 @@ namespace :vienna_airport do
   task get_departing_flight: :environment do
     # Fetch and parse HTML document
     base_url = Rails.application.secrets.scraping_website_url
+    # Get data to base_url
     data = FlightDepartureService.new(base_url).web_page_data
-
     sleep 10
-    # create city record with latitude, longitude in database
+
+    # Create city's temperature note record in database
     DestinationService.new(data).create_city_record
-    # get temperature of city and create note
-    CreateTemperatureNoteJob.new.perform
   end
 end
